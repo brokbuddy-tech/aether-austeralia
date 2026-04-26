126430249b901a8307d55ebb53fa4a7e29f225ff
PRIYANSHU SONI <devops@brokbuddy.com>
2026-04-26 15:03:30 +0530

    cc

--- next.config.ts
+++ next.config.ts
@@ -57,6 +57,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.brokbuddy.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
